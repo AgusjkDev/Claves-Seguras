431f6ef23785cdcdd83818714b0a9a64bffce033
Agustín Arnoldi <ShadeDev7@gmail.com>
2022-12-04 15:25:10 -0300

    Update regularExpressions.js

--- src/data/regularExpressions.js
+++ src/data/regularExpressions.js
@@ -1,2 +1,2 @@
 export const strongestPassword = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)(?=.*?[\W_]).{8,}$/; // At least 8 characters, 1 number, 1 uppercase letter, 1 lowercase letter, and one symbol
-export const strongPassword = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[\d\w\W]{8,}$/; // At least 8 characters, 1 number, and 1 letter
+export const strongPassword = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[\d\w\W]{8,}$/; // At least 8 characters, 1 number, 1 uppercase letter and 1 lowercase letter.
